61c653e9f7a027ede441e49972f13d8e4e416859
mauroscarlos <62101537+mauroscarlos@users.noreply.github.com>
2026-01-12 19:32:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
+import streamlit as st
+from sqlalchemy import create_engine
+import pandas as pd
+
+st.title("🛡️ SGF PRO - Teste de Conexão")
+
+# Função de conexão
+@st.cache_resource
+def conexao():
+    url = st.secrets["connections"]["postgresql"]["url"]
+    return create_engine(url)
+
+try:
+    engine = conexao()
+    # Tenta ler a tabela de usuários que você acabou de criar
+    df = pd.read_sql("SELECT nome FROM usuarios", engine)
+    st.success(f"Conectado com sucesso! Usuários encontrados: {df['nome'].iloc[0]}")
+except Exception as e:
+    st.error(f"Erro de conexão: {e}")
 
